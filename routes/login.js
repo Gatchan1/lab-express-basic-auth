7a8e6515dfcee2b26ee108fb188a611c170bbbcb
Raquel Barrio <r.barrio.f@gmail.com>
2023-05-10 18:57:29 +0200

    just implemented req.session

--- routes/login.js
+++ routes/login.js
@@ -4,8 +4,40 @@ const bcrypt = require("bcryptjs");
 const saltRounds = 14;
 
 router.get("/login", (req, res, next) => {
-
     res.render("login")
+})
+
+router.post("/login", (req, res, next) => {
+    let {username, password} = req.body;
+    let loginData = {username, password}
+
+    if(username == "" || password == "") {
+        loginData.errorMessage = "You can't leave any blank fields"
+        res.render("login", loginData)
+        return;
+    }
+
+    User.find({username})
+    .then(users => {
+        if (users.length == 0) {
+            loginData.errorMessage = "Wrong username and/or password"
+            res.render("login", loginData)
+            return;
+        }
+        let userDB = users[0];
+        if (bcrypt.compareSync(password, userDB.password)) {
+            req.session.currentUser
+        }
+
+        
+        
+    })
+    .catch(err => next(err))
+
+   
+
+      
+
 })
 
 
--- routes/signup.js
+++ routes/signup.js
@@ -4,52 +4,53 @@ const bcrypt = require("bcryptjs");
 const saltRounds = 14;
 
 router.get("/signup", (req, res, next) => {
-    res.render("signup")
-  })
-  
+  res.render("signup");
+});
+
 router.post("/signup", (req, res, next) => {
-    // console.log(req.body)
-    let {username, password, passwordRepeat} = req.body
-    let data = {
-      username, password, passwordRepeat
+  // console.log(req.body)
+  let { username, password, passwordRepeat } = req.body;
+  let singUpData = {
+    username,
+    password,
+    passwordRepeat,
+  };
+
+  if (username == "" || password == "" || passwordRepeat == "") {
+    singUpData.errorMessage = "You can't leave any blank fields";
+    res.render("signup", singUpData);
+    return;
     }
-  
-    if(username == "" || password == "" || passwordRepeat == "") {
-      data.errorMessage = "You can't leave any blank fields"
-      res.render("signup", data)
-      return;
-    } else if (password != passwordRepeat) {
-      data.errorMessage = "The passwords should match"
-      res.render("signup", data)
-      return;
-    }
-  
-    User.find({username})
-    .then(users => {
-      if(users.length != 0) {
-        data.errorMessage = "Username alreadey registered"
-        res.render("signup", data)
+  if (password != passwordRepeat) {
+    singUpData.errorMessage = "The passwords should match";
+    res.render("signup", singUpData);
+    return;
+  }
+
+  User.find({ username })
+    .then((users) => {
+      if (users.length != 0) {
+        singUpData.errorMessage = "Username alreadey registered";
+        res.render("signup", singUpData);
         return;
       }
     })
-    .catch(err => next(err))
-  
-    //If we have got this far without "falling" in any of the returns, then we are ready to really register our user in the database.
-    //(Hi Yabel!)
-    //First, let's encrypt the password.
+    .catch((err) => next(err));
 
-    const salt = bcrypt.genSaltSync(saltRounds);
-    const passwordEncrypted = bcrypt.hashSync(password, salt);
-  
-    // console.log("passw encrypted: ",passwordEncrypted)
-    User.create({username, password: passwordEncrypted})
-    .then(user => {
-        // console.log("user created!!", user)
-        res.redirect("/login")
+  //If we have got this far without "falling" in any of the returns, then we are ready to really register our user in the database.
+  //(Hi Yabel!)
+  //First, let's encrypt the password.
+
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const passwordEncrypted = bcrypt.hashSync(password, salt);
+
+  // console.log("passw encrypted: ",passwordEncrypted)
+  User.create({ username, password: passwordEncrypted })
+    .then((user) => {
+      // console.log("user created!!", user)
+      res.redirect("/login");
     })
-    .catch(err => next(err))
-  
-  })
+    .catch((err) => next(err));
+});
 
-
-  module.exports = router;
+module.exports = router;
