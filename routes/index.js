6ca940fcca755d5dc8e1fbf31cccbe7a313e84ac
Raquel Barrio <r.barrio.f@gmail.com>
2023-05-10 17:31:02 +0200

    iteration 1git add .

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,5 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-//A partir de aquí las rutas que he definido yo:
-// SIGN UP
-router.get("/signup", (req, res, next) => {
-  //la carpeta de new-views es solo para agrupar las que he definido yo
-  res.render("new-views/signup")
-
-})
-
-
-
 
 module.exports = router;
